7468432a39e5872bf2c6486931add0d9bdc5ce38
Chris Valle <chrisvalle35@me.com>
2020-09-14 13:43:39 -0400

    feat: create files based on input name

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 const path = require('path');
 const inquirer = require('inquirer');
-const { writeFile } = require('fs');
+const fs = require('fs');
 
-function createFileStructure() {
-  const file = writeFile('./', '', (err, data) => {
-    console.log(data);
+const SAMPLE_FILE_CONTENTS = `console.log('Overwrite Me! ${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}')`;
+
+const REDUX_FILE_MAP = [
+  { fileName: 'actions.js', fileData: SAMPLE_FILE_CONTENTS },
+  { fileName: 'reducer.js', fileData: SAMPLE_FILE_CONTENTS },
+  { fileName: 'actionTypes.js', fileData: SAMPLE_FILE_CONTENTS },
+];
+
+async function createFileStructure(folderName = 'Example') {
+  try {
+    REDUX_FILE_MAP.forEach(async (file) => {
+      const filePath = `./store/${folderName}/${file.fileName}`;
+      await fs.promises.writeFile(filePath, file.fileData);
+    });
     return true;
-  });
-  return false;
+  } catch (err) {
+    console.error(err);
+    return false;
+  }
 }
 
 function getCurrentDir() {
@@ -18,8 +31,33 @@ function getCurrentDir() {
   return dirName;
 }
 
-function createFolderByName(folderName) {
-  return true;
+async function createFolderByName(folderPath = './store/Example') {
+  console.log('Creating New Folder...');
+  try {
+    await fs.promises.mkdir(`${folderPath}`, { recursive: true });
+    return true;
+  } catch (error) {
+    console.error('Error Creating New Folder...', error);
+    return false;
+  }
+}
+
+async function doesFolderExist(folderPath = `${process.cwd()}/store/Example`) {
+  try {
+    console.log('FolderPath', folderPath);
+    const doesExist = await fs.promises.access(folderPath);
+    if (doesExist === true) {
+      console.log('Folder Exists');
+      // const acc = await fs.promises.access(folderPath);
+      // console.log(acc);
+      return true;
+    }
+  } catch (error) {
+    console.log('Folder does not exist', error);
+    const newFolder = await createFolderByName(folderPath);
+    console.log(newFolder);
+    return newFolder;
+  }
 }
 
 const start = () => {
@@ -52,23 +90,35 @@ const start = () => {
       },
       {
         type: 'input',
-        name: 'file_name',
+        name: 'fileName',
         message: 'What is the name of the file',
         default: function () {
           return 'Example';
         },
       },
     ])
-    .then((answers) => {
-      // Use user feedback for... whatever!!
+    .then(async (answers) => {
       console.log(JSON.stringify(answers, null, '  '));
-      const dir = getCurrentDir();
+      if (answers.fileType === 'Redux') {
+        console.log('Creating Redux Files...');
+      }
+      const folderPath = `./store/${answers.fileName}/`;
+      console.log('folderPath');
+      console.log(folderPath);
+      const doesExist = await doesFolderExist(folderPath);
+      // console.log('doesExist');
+      // console.log(doesExist);
+      const fileCreationResultBool = await createFileStructure(answers.fileName);
+      console.log('Files successfully created: ', fileCreationResultBool);
+      return fileCreationResultBool;
     })
     .catch((error) => {
       if (error) {
         // Prompt couldn't be rendered in the current environment
+        console.error(error);
       } else {
         // Something else when wrong
+        console.error('Disaster Struck');
       }
     });
 };
